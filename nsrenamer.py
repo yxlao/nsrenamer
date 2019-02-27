f665c0bdbe8daca377f70a408d409846f6f20849
Yixing Lao <yixing.lao@gmail.com>
2019-02-26 19:18:22 -0800

    better exclude files

--- nsrenamer.py
+++ nsrenamer.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 import subprocess
 from line import process_line
+import tempfile
+from shutil import copyfile
+from pprint import pprint
 
 
 def format_file(file_path):
     cmd = ["/usr/bin/clang-format-5.0", "-i", file_path]
+    subprocess.run(cmd)
+
+
+def git_reset(root_dir):
+    cmd = ["git", "-C", str(root_dir), "reset", "--hard", "HEAD"]
     subprocess.run(cmd)
 
 
@@ -22,8 +30,7 @@ def process_file(file_path, object_names, name_space):
         processed_lines.append(processed_line)
         if processed_line != line:
             changed = True
-        if object_names[0] in line or object_names[1] in line:
-            print(line.strip())
+            print(f"[Changed] {file_path}\n{line.strip()}\n{processed_line.strip()}\n")
 
     # Write
     with open(file_path, "w") as f:
@@ -45,6 +52,13 @@ def rename_namesapce(object_names, name_space, include_dirs, exclude_files):
         process_file(target_file, object_names, name_space)
 
 
+def glob_cpp_and_h_in_folder(foder_path):
+    files = list(foder_path.glob("**/*.cpp"))
+    files.extend(list(foder_path.glob("**/*.h")))
+    files = [str(f) for f in files]
+    return files
+
+
 if __name__ == "__main__":
     root_dir = Path.home() / "repo" / "Open3D"
     include_dirs = [
@@ -52,11 +66,20 @@ if __name__ == "__main__":
         root_dir / "examples",
         root_dir / "docs" / "_static" / "C++",
     ]
-    exclude_files = {
-        "/home/ylao/repo/Open3D/src/Open3D/Camera/PinholeCameraIntrinsic.cpp",
-        "/home/ylao/repo/Open3D/src/Open3D/Camera/PinholeCameraIntrinsic.h",
-    }
+
+    exclude_files = glob_cpp_and_h_in_folder(Path("/home/ylao/repo/Open3D/src/Open3D/Camera"))
+    pprint(exclude_files)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for exclude_file in exclude_files:
+            temp_exclude_file = Path(temp_dir + exclude_file)
+            temp_exclude_file.parent.mkdir(parents=True, exist_ok=True)
+            copyfile(exclude_file, temp_exclude_file)
+        git_reset(root_dir)
+        for exclude_file in exclude_files:
+            temp_exclude_file = Path(temp_dir + exclude_file)
+            copyfile(temp_exclude_file, exclude_file)
+
     object_names = ["PinholeCameraIntrinsic", "PinholeCameraIntrinsicParameters"]
     name_space = "camera"
 
-    rename_namesapce(object_names, name_space, include_dirs, exclude_files)
+    # rename_namesapce(object_names, name_space, include_dirs, exclude_files)
