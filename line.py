3cef06968aae06e4c592c80b2cbd6aeca434d1c6
Yixing Lao <yixing.lao@gmail.com>
2019-02-27 00:58:16 -0800

    clean up

--- line.py
+++ line.py
@@ -1,13 +1,5 @@
 import re
 
-# for object_name in object_names:
-#     line = 'TEST(PinholeCameraIntrinsic, Constructor_Kinect2ColorCameraDefault) {'
-#     line = 'look, "PinholeCameraIntrinsic" is good'
-#     # regex = r'(?!%s|%s|%s)(%s)' % (f'{object_name}.h', f'"{object_name}"', f'TEST\({object_name}', object_name)
-#     regex = r'(?!"PinholeCameraIntrinsic")(PinholeCameraIntrinsic)'
-#     line_sub = re.sub(regex, f'{namespace}::{object_name}', line)
-# return line
-
 enum_classes = {
     "PinholeCameraIntrinsicParameters",
     "GeometryType",
