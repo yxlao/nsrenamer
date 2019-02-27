594ef997e33991bcc3fc9b236c91c6f9e46c6190
Yixing Lao <yixing.lao@gmail.com>
2019-02-26 18:49:51 -0800

    back and forth framework

--- line.py
+++ line.py
@@ -1,16 +1,50 @@
 import re
 
-def process_line(line, object_names, name_space):
+# for object_name in object_names:
+#     line = 'TEST(PinholeCameraIntrinsic, Constructor_Kinect2ColorCameraDefault) {'
+#     line = 'look, "PinholeCameraIntrinsic" is good'
+#     # regex = r'(?!%s|%s|%s)(%s)' % (f'{object_name}.h', f'"{object_name}"', f'TEST\({object_name}', object_name)
+#     regex = r'(?!"PinholeCameraIntrinsic")(PinholeCameraIntrinsic)'
+#     line_sub = re.sub(regex, f'{name_space}::{object_name}', line)
+# return line
+
+def process_line_one(line, object_name, name_space):
+    print(f"[init       ]: {line}")
+    line = re.sub(rf'{object_name}\b', rf'{name_space}::{object_name}', line)
+    print(f"[all_replace]: {line}")
+    line = re.sub(rf'{name_space}::{object_name}\.h', rf'{object_name}.h', line)
+    print(f"[back_header]: {line}")
+    line = re.sub(rf'"{name_space}::{object_name}"', rf'"{object_name}"', line)
+    print(f"[back_quote ]: {line}")
+    print()
     return line
 
-    # for object_name in object_names:
-    #     line = 'TEST(PinholeCameraIntrinsic, Constructor_Kinect2ColorCameraDefault) {'
-    #     line = 'look, "PinholeCameraIntrinsic" is good'
-    #     # regex = r'(?!%s|%s|%s)(%s)' % (f'{object_name}.h', f'"{object_name}"', f'TEST\({object_name}', object_name)
-    #     regex = r'(?!"PinholeCameraIntrinsic")(PinholeCameraIntrinsic)'
-    #     line_sub = re.sub(regex, f'{name_space}::{object_name}', line)
-    # return line
+
+def process_line(line, object_names, name_space):
+    new_line = line
+    for object_name in object_names:
+        new_line = process_line_one(line, object_name, name_space)
+    return new_line
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    before_lines = [
+        '#include <Open3D/Camera/PinholeCameraIntrinsic.h>',
+        'py::class_<PinholeCameraIntrinsic> pinhole_intr(m, "PinholeCameraIntrinsic",',
+        '"PinholeCameraIntrinsic");',
+        'py::detail::bind_default_constructor<PinholeCameraIntrinsic>(pinhole_intr);',
+        'TEST(PinholeCameraIntrinsic, GetFocalLength) {',
+        'PinholeCameraIntrinsicParameters::Kinect2ColorCameraDefault)'
+    ]
+    after_lines = [
+        '#include <Open3D/Camera/PinholeCameraIntrinsic.h>',
+        'py::class_<camera::PinholeCameraIntrinsic> pinhole_intr(m, "PinholeCameraIntrinsic",',
+        '"PinholeCameraIntrinsic");',
+        'py::detail::bind_default_constructor<camera::PinholeCameraIntrinsic>(pinhole_intr);',
+        'TEST(PinholeCameraIntrinsic, GetFocalLength) {',
+        'PinholeCameraIntrinsicParameters::Kinect2ColorCameraDefault)'
+    ]
+
+    for before_line in before_lines:
+        process_line_one(before_line, 'PinholeCameraIntrinsic', 'camera')
+
--- nsrenamer.py
+++ nsrenamer.py
@@ -2,17 +2,16 @@ from pathlib import Path
 import subprocess
 from line import process_line
 
+
 def format_file(file_path):
-    cmd = ['/usr/bin/clang-format-5.0',
-           '-i',
-           file_path]
+    cmd = ["/usr/bin/clang-format-5.0", "-i", file_path]
     subprocess.run(cmd)
 
 
 def process_file(file_path, object_names, name_space):
     # Read
     # print(f"Processing {file_path}")
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         lines = f.readlines()
 
     # Process
@@ -27,7 +26,7 @@ def process_file(file_path, object_names, name_space):
             print(line.strip())
 
     # Write
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         for processed_line in processed_lines:
             f.write(processed_line)
 
@@ -48,7 +47,11 @@ def rename_namesapce(object_names, name_space, include_dirs, exclude_files):
 
 if __name__ == "__main__":
     root_dir = Path.home() / "repo" / "Open3D"
-    include_dirs = [root_dir / "src", root_dir / "examples", root_dir / "docs" / "_static" / "C++"]
+    include_dirs = [
+        root_dir / "src",
+        root_dir / "examples",
+        root_dir / "docs" / "_static" / "C++",
+    ]
     exclude_files = {
         "/home/ylao/repo/Open3D/src/Open3D/Camera/PinholeCameraIntrinsic.cpp",
         "/home/ylao/repo/Open3D/src/Open3D/Camera/PinholeCameraIntrinsic.h",
